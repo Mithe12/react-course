c2c1045ed7594f5bf6990857028ab5a7ef251f84
Mithelash <mithelash.c@hotmail.com>
2023-01-07 17:58:48 +0000

    Functional Component and PropTypes

--- Project/BattleApp/app/Component/Popular.jsx
+++ Project/BattleApp/app/Component/Popular.jsx
@@ -1,4 +1,27 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
+
+// we are passing the current state and function that changes the state.
+// Here state is handled by parent component -- declarative thinking
+function LanguagesNav({selected, onUpdateLanguage}){
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    return (
+        <select onChange= {(e) => (onUpdateLanguage(e.target.value))}
+                selected = {selected} >
+                    {languages.map((lang) => (
+                        <option key = {lang} value = {lang}> 
+                            {lang} 
+                        </option>
+                    ))}
+        </select>
+    );
+}
+
+// we defined the Proptypes for Component, Type Check 
+LanguagesNav.propTypes = {
+    selected: PropTypes.string.isRequired,
+    onUpdateLanguage: PropTypes.func.isRequired,
+}
 
 export default class Popular extends React.Component {
 
@@ -18,18 +41,16 @@ export default class Popular extends React.Component {
     }
 
     render(){
-        const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+        // paramters for the functional component
+        const {selectedLanguage} = this.state;
+        const updateLanguage = this.updateLanguage;
+
         return(
             <main>
-                <select 
-                    onChange= {(e) => (this.updateLanguage(e.target.value))}
-                    selected = {this.state.selectedLanguage} >
-                        {languages.map((lang) => (
-                            <option key = {lang} value = {lang}> 
-                                {lang} 
-                            </option>
-                        ))}
-                </select>
+                <LanguagesNav 
+                    selected = {selectedLanguage}
+                    onUpdateLanguage = {updateLanguage}
+                 />
                 {JSON.stringify(this.state, null, 2)}
             </main>
         );
